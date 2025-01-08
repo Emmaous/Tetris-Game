f5069df16fd49b9f004b537db0d5d1bc17d19b29
Emmanuel Oyiwona <emmaogah97@gmail.com>
2025-01-08 17:23:19 +0000

    game grid #01
    declaring variables and properties to define the game grid

--- Tetris Game/GameGrid.cs
+++ Tetris Game/GameGrid.cs
@@ -8,6 +8,19 @@ namespace Tetris_Game
 {
     public class GameGrid
     {
+        // 2-D rectangular array to reprsent game grid
+        private readonly int[,] grid;
 
+        // rows property
+        public int Rows { get; }
+        //column property
+        public int Columns { get; }
+
+        // defining indexer to provide easy access to grid array
+        public int this [int row, int column] 
+        { 
+            get => grid [row, column];
+            set => grid [row, column] = value;
+        }
     }
 }
