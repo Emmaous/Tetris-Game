c028f35a8ad7817114e9f15f13e2dac9be124b3a
Emmanuel Oyiwona <emmaogah97@gmail.com>
2025-01-09 13:11:08 +0000

    GameGrid class update
    created the constructor for the class and some convenience methods for game checks

--- Tetris Game/GameGrid.cs
+++ Tetris Game/GameGrid.cs
@@ -22,5 +22,48 @@ namespace Tetris_Game
             get => grid [row, column];
             set => grid [row, column] = value;
         }
+
+        //constructor
+        public GameGrid(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+            grid = new int[Rows, Columns];
+        }
+
+        //Convenience Methods
+        //check for presence of a given row or column
+        public bool IsInside(int row, int column)
+        {
+            return row >= 0 && row < Rows && column >= 0 && column < Columns;
+        }
+
+        //check that a given cell is empty
+        public bool IsEmpty(int row, int column)
+        {
+            return IsInside(row, column) && grid[row, column] == 0;
+        }
+
+        //check if row is full
+        public bool IsRowFull(int row)
+        {
+            for (int column = 0; column < Columns; column++)
+            {
+                if (grid[row, column] == 0) return false;
+            }
+
+            return true;
+        }
+
+        //check if row is empty
+        public bool IsRowEmpty(int row)
+        {
+            for (int column = 0; column < Columns; column++)
+            {
+                if (grid[row, column] != 0) return false;
+            }
+
+            return true;
+        }
     }
 }
