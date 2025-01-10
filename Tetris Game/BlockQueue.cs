e2280c4d3df251c65d18bd380419de1b3ff98c67
Emmanuel Oyiwona <emmaogah97@gmail.com>
2025-01-10 17:32:19 +0000

    Created BlockQueue class
    created a class to handle the generation of new blocks

---
+++ Tetris Game/BlockQueue.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris_Game
+{
+    public class BlockQueue
+    {
+        // array containing instance of all block sub-classes
+        public readonly Block[] blocks = new Block[]
+        {
+            new IBlock(), new JBlock(), new LBlock(), new OBlock(), new SBlock(), new TBlock(), new ZBlock()
+        };
+
+        private readonly Random random = new Random();
+        //property to represent next block in queue
+        public Block NewBlock {  get; private set; }
+        
+        //constructor
+        public BlockQueue()
+        {
+            NewBlock = RandomBlock();
+        }
+        
+        //method to return a random block
+        private Block RandomBlock ()
+        {
+            return blocks[random.Next(blocks.Length)];
+        }
+
+        //method to return new block and update property
+        public Block GetAndUpdate()
+        {
+            Block block = NewBlock;
+
+            do //loop till a different block
+            {
+                NewBlock = RandomBlock();
+            }
+            while (block.blockId == NewBlock.blockId);
+
+            return block;
+        }
+
+    }
+}
